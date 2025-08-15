d00c61ad8fdc5720545450e82554310fb1c485fb
Laden8210 <domingo.laden@gmail.com>
2025-08-16 06:48:10 +0800

    major revision

--- public/build/assets/text-link-BSCl0tmz.js
+++
@@ -1,1 +0,0 @@
-import{j as t,$ as n}from"./app-BvO3OH2q.js";import{b as a}from"./button-DfXw9gWu.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
