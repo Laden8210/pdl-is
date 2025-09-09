5d5f7e03c31b8a4cafb5ae9c33c97f47f188de33
Laden8210 <domingo.laden@gmail.com>
2025-09-09 17:19:52 +0800

    update

---
+++ public/build/assets/input-error-Cl26T7g4.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-ChwqrLNZ.js";import{b as o}from"./utils-DcuYUYn6.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
